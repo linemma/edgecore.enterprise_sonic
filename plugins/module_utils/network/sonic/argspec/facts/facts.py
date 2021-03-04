97cb76f0a7e6aa1b69f41619794ffc1643f1aa8e
Emma Lin <emma_lin@edge-core.com>
2021-03-04 16:50:52 +0800

    1st

---
+++ plugins/module_utils/network/sonic/argspec/facts/facts.py
@@ -0,0 +1,36 @@
+#
+# -*- coding: utf-8 -*-
+# Copyright 2020 Dell Inc. or its subsidiaries. All Rights Reserved
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+"""
+The arg spec for the sonic facts module.
+"""
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+class FactsArgs(object):  # pylint: disable=R0903
+
+    """ The arg spec for the sonic facts module
+    """
+
+    def __init__(self, **kwargs):
+        pass
+
+    choices = [
+        'all',
+        'vlans',
+        'interfaces',
+        'l2_interfaces',
+        'l3_interfaces',
+        'lag_interfaces',
+    ]
+
+    argument_spec = {
+        'gather_subset': dict(default=['!config'], type='list', elements='str'),
+        'gather_network_resources': dict(choices=choices,
+                                         type='list',
+                                         elements='str'),
+    }
